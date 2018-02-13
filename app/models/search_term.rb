2812e062d9142c070f141bf78dba0ed8f1268c10
Evan Dancer <evan@cribspot.com>
2018-02-13 15:11:04 -0500

    Fix downcase for search_term before save

--- app/models/search_term.rb
+++ app/models/search_term.rb
@@ -1,2 +1,12 @@
+# @class SearchTerm
 class SearchTerm < ApplicationRecord
+  before_save :downcase_name
+
+  def self.find_by_name(name)
+    super(name.downcase)
+  end
+
+  def downcase_name
+    name.downcase!
+  end
 end
